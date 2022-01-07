29495b3bcf167114c9909adbab0fbf37d0d2c814
Arvin <exarvin@gmail.com>
2022-01-07 14:33:31 +0800

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-b4a07118bbca0aa8d6a6.js"
+    "url": "webpack-runtime-2c404501462860a7cf7f.js"
   },
   {
     "url": "framework-cd3e1e804d552fa282ef.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "568f3874af04e8a3419604acab3719e0"
+    "revision": "bcb363be887023781560d85cde84c210"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
